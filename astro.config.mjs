b727e8c2eed01e78ead30f01487ecbecf6d99fdb
Mateo Hrastnik <mateo@lloyds-digital.com>
2024-12-03 13:11:12 +0100

    Add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  adapter: vercel({
+    isr: false,
+    edgeMiddleware: false,
+    webAnalytics: { enabled: false },
+    maxDuration: 15,
+  }),
+  integrations: [tailwind()],
 });
